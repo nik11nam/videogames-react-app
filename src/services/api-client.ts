bafcef4fc2aa8cf4109163420c822b4ee5f82000
nikx <nik.koshi@gmail.com>
2023-11-11 16:21:20 +0800

    Update the Api key in api-client

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -6,7 +6,7 @@ import axios from "axios";
 const apiClient = axios.create({
     baseURL: "https://api.rawg.io/api",
     params: {
-        key: '5860febe715745bbb82886b2e583e757'
+        key: 'xxxxx'
     }
 });
 
